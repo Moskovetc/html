ca31b9e08b6abe7b28fb237faf35e28b2d0ec42f
moskovetc <moskovetc@hotmail.com>
2018-12-17 15:30:51 +0400

    hidden divs

--- scripts/product.js
+++ scripts/product.js
@@ -1,3 +1,7 @@
-function productInfo() {
-    alert("worked");
+function productInfo(block_4_close, block_4_open) {
+    $("html,body").animate({
+        scrollTop: $('.product-container-onclick ').offset().top - 150
+    }, 100);
+    document.getElementById(block_4_close).style.display='none';
+    document.getElementById(block_4_open).style.display='flex';
 }
