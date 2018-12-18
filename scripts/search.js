45b9a841bda78fe50cd00dae1ff36e93c22da895
moskovetc <moskovetc@hotmail.com>
2018-12-18 17:48:37 +0400

    todo filter products

--- scripts/search.js
+++ scripts/search.js
@@ -1,1 +1,26 @@
-// showModal('search-modal', 'search-button');
+function showProducts() {
+    var elems = document.getElementsByClassName('product-container');
+    for (var i = 0; i < elems.length; i++) {
+        document.getElementsByClassName('products-list')[0].appendChild(elems[i]);
+    }
+}
+
+function filterProducts() {
+      // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
